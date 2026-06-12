2c9bd3cc6a7396be33a5c4be195a6c6d464204f6
Abeerr0 <abeer.ali.alsaiari1@gmail.com>
2026-06-12 11:21:36 +0000

    Complete More Data Structures and Exceptions tasks

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    for i in range(len(roman_string)):
+        if i > 0 and rom.get(roman_string[i], 0) > rom.get(roman_string[i - 1], 0):
+            res += rom.get(roman_string[i], 0) - 2 * rom.get(roman_string[i - 1], 0)
+        else:
+            res += rom.get(roman_string[i], 0)
+    return res

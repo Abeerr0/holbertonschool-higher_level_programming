2b84da3d901929478a8da311051a690c5aa45b8e
Abeer Ali. <abeer.ali.alsaiari1@gmail.com>
2026-06-13 20:57:50 +0300

    Enhance lazy_matrix_mul with input checks
    Added input validation for matrix multiplication parameters.

--- 101-lazy_matrix_mul.py
+++ 101-lazy_matrix_mul.py
@@ -1,8 +1,33 @@
-#!/usr/bin/python3
-"""Module for lazy_matrix_mul method."""
 import numpy as np
 
-
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies 2 matrices by using the module NumPy."""
-    return np.matmul(m_a, m_b)
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if m_a == [] or (len(m_a) > 0 and m_a[0] == []):
+        raise ValueError("m_a can’t be empty")
+    if m_b == [] or (len(m_b) > 0 and m_b[0] == []):
+        raise ValueError("m_b can’t be empty")
+    for row in m_a:
+        if not isinstance(row, list):
+            raise TypeError("m_a must be a list of lists") # أو حسب متطلباتك
+        for x in row:
+            if not isinstance(x, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    
+    for row in m_b:
+        if not isinstance(row, list):
+            raise TypeError("m_b must be a list of lists")
+        for x in row:
+            if not isinstance(x, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must should be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must should be of the same size")
+    try:
+        return np.matmul(m_a, m_b)
+    except ValueError:
+        raise ValueError("ma and mb can’t be multiplied")

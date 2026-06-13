dcbaa8747ff205c7b5f827a69f9270a9c7a5f104
Abeerr0 <abeer.ali.alsaiari1@gmail.com>
2026-06-13 18:43:44 +0000

    Fix Task 7: Remove manual checks and let NumPy handle exceptions

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,28 +9,4 @@ def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies 2 matrices by using NumPy.
     """
-    if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
-    for row in m_a:
-        if type(row) is not list:
-            raise TypeError("m_a must be a list of lists")
-        for el in row:
-            if type(el) not in (int, float):
-                raise TypeError("m_a should contain only integers or floats")
-
-    for row in m_b:
-        if type(row) is not list:
-            raise TypeError("m_b must be a list of lists")
-        for el in row:
-            if type(el) not in (int, float):
-                raise TypeError("m_b should contain only integers or floats")
-
     return np.matmul(m_a, m_b)

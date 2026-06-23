448b459b2927e4a284a1ab674f913521c4889a40
Abeerr0 <abeer.ali.alsaiari1@gmail.com>
2026-06-23 07:40:25 +0000

    Task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Module that defines a full Square class inheriting from Rectangle."""
+"""Module for Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class that defines a complete square, inherits from Rectangle."""
+    """Square class."""
 
     def __init__(self, size):
-        """Initializes Square with a validated size."""
+        """Initializer."""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
-        """Returns the string representation of the square."""
+        """String representation."""
         return "[Square] {}/{}".format(self.__size, self.__size)

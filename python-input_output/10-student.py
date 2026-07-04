e098226b4f834ddbf951db3d52729c2c01bde2c3
Abeer Ali. <abeer.ali.alsaiari1@gmail.com>
2026-07-04 20:30:52 +0300

    Refactor to_json method and enhance docstring
    Updated the to_json method to improve attribute filtering and added detailed docstring.

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,14 +4,21 @@
 
 class Student:
     """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance."""
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return the dictionary representation of the instance."""
-        if type(attrs) is list:
-            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        """Return a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attribute names contained
+        in this list are retrieved. Otherwise, all attributes are
+        retrieved.
+        """
+        if type(attrs) is list and all(type(attr) is str for attr in attrs):
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
         return self.__dict__

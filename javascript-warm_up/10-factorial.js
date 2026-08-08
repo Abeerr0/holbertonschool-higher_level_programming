abadcbc81ec19902788f27cff46acad598d68cc1
Abeer Ali. <abeer.ali.alsaiari1@gmail.com>
2026-08-08 20:41:21 +0300

    javascript-warm_up

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
-
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(parseInt(process.argv[2])));

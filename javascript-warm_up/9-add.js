ced7b28d2080ccc8130da4ac299ea863bee65276
Abeer Ali. <abeer.ali.alsaiari1@gmail.com>
2026-08-08 20:37:54 +0300

    javascript-warm_up

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,6 +2,5 @@
 function add (a, b) {
   return a + b;
 }
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-console.log(add(a, b));
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));

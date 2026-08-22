bde3e4916ed01ec790fbc4612aa97061466e2971
Abeer Ali. <abeer.ali.alsaiari1@gmail.com>
2026-08-22 15:20:33 +0300

    Update __slots__ definition in LockedClass
    Changed __slots__ from a tuple to a list for consistency.

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Defines a LockedClass."""
+
+
 class LockedClass:
-    __slots__ = ('first_name',)
+    """Prevent dynamic creation of instance attributes except first_name."""
+
+    __slots__ = ["first_name"]
